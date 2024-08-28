d5ed467de59abbda81e7881f542a15d0381fa4b5
Sahil More <skm842011@gmail.com>
2024-08-29 00:14:18 +0530

    Update Merge_sort.py

--- Merge_sort.py
+++ Merge_sort.py
@@ -1,20 +1,22 @@
-def f(s,s1):
-    if s>=s1:
-        s-=1
-        print(s,s1,"-------------------------------------------------------------------------------------------------------")
+def f(s, s1):
+    if s >= s1:
+        s -= 1
+        print("---------------------------------------------------",s,
+              "-------------------------------------------------------------------------------------------------------")
 
     else:
+        print("---------------------------------------------------", "X",
+              "-------------------------------------------------------------------------------------------------------")
+
         return None
 
-    print("reached here")
-    if s==0:
-        print("near death exp")
-        while True:
-            print("mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm")
-    print("reached here as welll")
-    f(s,s1)
-    print("reached here as welll ohhhhhhhhhhhhhhhhhh")
-    f(s,s1)
-    print("so is it just climbing ladder now")
-    #f(s,s1)
-f(15,3)
+    print("A")
+
+    f(s, s1)
+    print("B")
+    f(s, s1)
+    print("C")
+    # f(s,s1)
+
+
+f(4, 3)
