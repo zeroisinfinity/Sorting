3696ad5e884a8661149ae07d44f16fb4079f3657
Sahil More <skm842011@gmail.com>
2024-08-29 11:23:35 +0530

    Update Bubble_sort.py

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -1,1 +1,16 @@
-
+def bubble_sort(array):
+	for limiter in range(len(array)):
+		swap_flag = False
+		for cf in range( 0 , len(array) - limiter - 1 ):
+			if array[cf]>array[cf+1]:
+				array[cf],array[cf+1] = array[cf+1],array[cf]
+				swap_flag = True
+		if swap_flag == False:
+			break
+	return array
+	
+print(bubble_sort([9,8,7,6,5,4,3,2,1,0]))
+			
+			
+			
+	
