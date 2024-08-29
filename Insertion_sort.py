f0678d1e9f864c309f55fcbff7edfcbce415e248
Sahil More <skm842011@gmail.com>
2024-08-29 19:45:25 +0530

    Update Insertion_sort.py

--- Insertion_sort.py
+++ Insertion_sort.py
@@ -1,8 +1,5 @@
 def insertion_sort(array):
-
-    if len(array) != 1 and len(array) != 0:
-
-        for test_sub in range(1,len(array),1):
+    for test_sub in range(1,len(array),1):
             sample = array[test_sub]
             compare = test_sub - 1
             while compare >= 0 and sample < array[compare]:
@@ -10,12 +7,6 @@ def insertion_sort(array):
                 compare -= 1
             array[compare+1] = sample
         return array
-
-    else:
-        if len(array)==1:
-            return 'Array already sorted'
-        else:
-            return 'Array has no elements'
 
 ins_array = [9,8,7,6,5,4,3,2,1,0]
 #ins_array = [9]
