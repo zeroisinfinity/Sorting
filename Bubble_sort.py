62e5b45a835fe54bcfd8f01d9495bda7fb176722
Sahil More <skm842011@gmail.com>
2024-08-29 11:22:15 +0530

    Update Bubble_sort.py

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -1,7 +1,1 @@
-def bubble_sort(sorted_array):
-    for compare in range(0,len(sorted_array),1):
-        for iter in range(compare+1,len(sorted_array),1):
-            if sorted_array[compare] > sorted_array[iter]:
-                sorted_array[compare],sorted_array[iter] = sorted_array[iter],sorted_array[compare]
-    return sorted_array
-print(bubble_sort([7,3,2,1,4]))
+
