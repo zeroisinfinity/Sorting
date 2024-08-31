22e19f16caefe71319d5e5af68c43ae940b02ac0
Sahil More <skm842011@gmail.com>
2024-08-31 23:00:40 +0530

    Add files via upload

---
+++ Quick_sort_HoaresFirstPivot.py
@@ -0,0 +1,36 @@
+import sys
+sys.setrecursionlimit(10000000)
+
+def quicksort(array, start_pos, end_pos):
+    if start_pos < end_pos:
+        new_pivot = partition(array, start_pos, end_pos)
+
+        quicksort(array, start_pos, new_pivot)
+        quicksort(array, new_pivot + 1, end_pos)
+
+
+def partition(array, start_pos, end_pos):
+    pivot = array[start_pos]
+    inc = start_pos - 1
+    dec = end_pos + 1
+
+    while True:
+
+        inc += 1
+        while array[inc] < pivot:
+            inc += 1
+
+        dec-=1
+        while array[dec] > pivot:
+            dec-=1
+        if inc >= dec:
+            return dec
+        array[inc] , array[dec] = array[dec] , array[inc]
+        #print(array)
+
+
+
+arr = [900] + [9]*2 + list(range(5,0,-1)) +  list(range(60,8,-3)) + [9]*4
+
+quicksort(arr,0,len(arr) - 1)
+print(arr)
---
+++ Quick_sort_LumotoRandomPivot.py
@@ -0,0 +1,35 @@
+import random
+
+
+def shuffle(array,start_pos,end_pos):
+    sh_pivot = random.randint(start_pos,end_pos)
+    array[sh_pivot] , array[start_pos] = array[start_pos] , array[sh_pivot]
+    return partition(array,start_pos,end_pos)
+
+def partition(array, start_pos, end_pos):
+    pivot = array[end_pos]
+    minimum = start_pos - 1
+
+    for indx in range(start_pos, end_pos ):
+        if array[indx] < pivot:
+            minimum += 1
+            array[minimum], array[indx] = array[indx], array[minimum]
+    array[minimum + 1], array[end_pos] = array[end_pos], array[minimum + 1]
+
+    return minimum + 1
+
+def quick_sort(array, start_pos, end_pos):
+
+    if start_pos < end_pos:
+        current_pivot = shuffle(array, start_pos, end_pos)
+        quick_sort(array, start_pos, current_pivot - 1)
+        quick_sort(array, current_pivot + 1, end_pos)
+
+arr = [9, 8, 1, 0, 11, 12, 66]
+quick_sort(arr, 0, len(arr)-1)
+print(arr)
+
+
+
+
+
