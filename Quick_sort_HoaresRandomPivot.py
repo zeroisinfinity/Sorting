8440a38511af6d7da69e80314a3faa17436288cf
Sahil More <skm842011@gmail.com>
2024-09-01 00:10:46 +0530

    Add files via upload

---
+++ Quick_sort_HoaresRandomPivot.py
@@ -0,0 +1,41 @@
+import sys
+import random
+sys.setrecursionlimit(10000000)
+
+def quicksort(array, start_pos, end_pos):
+    if start_pos < end_pos:
+        new_pivot = shuffle(array, start_pos, end_pos)
+
+        quicksort(array, start_pos, new_pivot)
+        quicksort(array, new_pivot + 1, end_pos)
+
+def shuffle(array,start_pos,end_pos):
+    sh_pivot = random.randint(start_pos,end_pos)
+    array[sh_pivot] , array[start_pos] = array[start_pos] , array[sh_pivot]
+    return partition(array,start_pos,end_pos)
+
+def partition(array, start_pos, end_pos):
+    pivot = array[start_pos]
+    inc = start_pos - 1
+    dec = end_pos + 1
+
+    while True:
+
+        inc += 1
+        while array[inc] < pivot:
+            inc += 1
+
+        dec-=1
+        while array[dec] > pivot:
+            dec-=1
+        if inc >= dec:
+            return dec
+        array[inc] , array[dec] = array[dec] , array[inc]
+        #print(array)
+
+
+
+arr = [900] + [9]*2 + list(range(5,0,-1)) +  list(range(60,8,-3)) + [9]*4
+
+quicksort(arr,0,len(arr) - 1)
+print(arr)
