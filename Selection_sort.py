c514010305b12d2e490cc8e6e03afa6fc7a850b6
Sahil More <skm842011@gmail.com>
2024-08-31 12:11:58 +0530

    Update Selection_sort.py

--- Selection_sort.py
+++ Selection_sort.py
@@ -1,5 +1,4 @@
 def selection_sort(array):
-
     for iter in range(0,len(array),1):
         current_min = array[iter]
         for compare in range(iter+1,len(array),1):
@@ -19,5 +18,5 @@ sel_array = [9,8,7,6,5,4,3,2,1,0]
 #sel_array = [9]
 #sel_array = []
 
-print(selection_sort(sel_array))
+print(selection_sort_python_touch(sel_array))
 
