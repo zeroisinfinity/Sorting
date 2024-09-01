abc26cd79a8f5b2010d238671e7589443dd721ef
Sahil More <skm842011@gmail.com>
2024-09-01 10:42:58 +0530

    Update insertion_sort_python_touch.py

--- insertion_sort_python_touch.py
+++ insertion_sort_python_touch.py
@@ -8,11 +8,11 @@ def insertion_sort_python_touch(array):
 
         for test_sub in range(1,len(array),1):
             sample = array[test_sub]
-            compare = test_sub - 1
-            while compare >= 0 and sample < array[compare]:
-                compare -= 1
-            array[compare+2:test_sub+1] = array[compare+1:test_sub]
-            array[compare + 1] = sample
+            cf = test_sub - 1
+            while cf >= 0 and sample < array[cf]:
+                cf -= 1
+            array[cf+2:test_sub+1] = array[cf+1:test_sub]
+            array[cf + 1] = sample
         return array
 
     else:
@@ -25,9 +25,3 @@ ins_array = [9,8,7,6,5,4,3,2,1,0]
 #ins_array = [9]
 #ins_array = []
 print(insertion_sort_python_touch(ins_array))
-
-
-
-
-
-
