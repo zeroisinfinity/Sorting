2822f80c1b4b2268f36e521b54b664f2d1375426
Sahil More <skm842011@gmail.com>
2024-09-01 10:40:44 +0530

    Update Selection_sort.py

--- Selection_sort.py
+++ Selection_sort.py
@@ -1,22 +1,24 @@
 def selection_sort(array):
-    for iter in range(0,len(array),1):
-        current_min = array[iter]
-        for compare in range(iter+1,len(array),1):
-            if current_min > array[compare]:
-                current_min = array[compare]
-                array[compare], array[iter] = array[iter] , current_min
+    for index in range(0, len(array), 1):
+        current_min = array[index]
+        for cf in range(index + 1, len(array), 1):
+            if current_min > array[cf]:
+                current_min = array[cf]
+                array[cf], array[index] = array[index], current_min
     return array
-    
-def selection_sort_python_touch(sorted_array):
-    for compare in range(0,len(sorted_array),1):
-        for iter in range(compare+1,len(sorted_array),1):
-            if sorted_array[compare] > sorted_array[iter]:
-                sorted_array[compare],sorted_array[iter] = sorted_array[iter],sorted_array[compare]
-    return sorted_array
-print(selection_sort([7,3,2,1,4]))
-sel_array = [9,8,7,6,5,4,3,2,1,0]
-#sel_array = [9]
-#sel_array = []
+
+def selection_sort_python_touch(array):
+    for index in range(0, len(array), 1):
+        for cf in range(index + 1, len(array), 1):
+            if array[index] > array[cf]:
+                array[index], array[cf] = array[cf], array[index]
+    return array
+
+
+print(selection_sort([7, 3, 2, 1, 4]))
+sel_array = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+# sel_array = [9]
+# sel_array = []
 
 print(selection_sort_python_touch(sel_array))
 
