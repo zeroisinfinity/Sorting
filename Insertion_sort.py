72f775f867c26f3506f9baf6430589d81ffe0aae
Sahil More <skm842011@gmail.com>
2024-09-01 10:41:49 +0530

    Update Insertion_sort.py

--- Insertion_sort.py
+++ Insertion_sort.py
@@ -1,20 +1,14 @@
 def insertion_sort(array):
     for test_sub in range(1,len(array),1):
             sample = array[test_sub]
-            compare = test_sub - 1
-            while compare >= 0 and sample < array[compare]:
-                array[compare + 1] = array[compare]
-                compare -= 1
-            array[compare+1] = sample
-        return array
+            cf = test_sub - 1
+            while cf >= 0 and sample < array[cf]:
+                array[cf + 1] = array[cf]
+                cf -= 1
+            array[cf+1] = sample
+    return array
 
 ins_array = [9,8,7,6,5,4,3,2,1,0]
 #ins_array = [9]
 #ins_array = []
 print(insertion_sort(ins_array))
-
-
-
-
-
-
